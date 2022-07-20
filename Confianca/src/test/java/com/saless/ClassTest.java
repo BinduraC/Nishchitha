956d4aa56b45c4b17c576cf6dbdc113f6244ffbc
Win10 <Win10@DESKTOP-8A44VM7>
2022-07-20 17:29:01 +0530

    commit2

---
+++ Confianca/src/test/java/com/saless/ClassTest.java
@@ -0,0 +1,17 @@
+package com.saless;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassTest {
+	
+	@Test
+	public void clssTest() {
+		Reporter.log("test1", true);
+		Reporter.log("test2", true);
+		Reporter.log("test3", true);
+		Reporter.log("test4", true);
+
+		System.out.println("Confiansa to master");
+	}
+
+}
