48b1b1b0461cf5c8156160b5dcf8815183024768
bindura123 <binduraqa@gmail.com>
2022-07-26 12:04:07 +0530

    hello

--- Confianca/src/test/java/com/saless/ClassTest.java
+++ Confianca/src/test/java/com/saless/ClassTest.java
@@ -7,20 +7,7 @@ public class ClassTest {
 	@Test
 	public void clssTest() {
 		Reporter.log("test1", true);
-		Reporter.log("test2", true);
-		Reporter.log("test3", true);
-		Reporter.log("test4", true);
-
-		System.out.println("Confiansa to master");
-		System.out.println("child change");//hi
-
-
-		System.out.println("changes on 21-07-22");//hi
-
-		System.out.println("child change");
-		System.out.println("kid change today ");//hi
-		System.out.println("kid change");
-		//sd
+		
 
 	}
 	
